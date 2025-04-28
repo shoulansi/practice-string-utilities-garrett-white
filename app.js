375a975ceda30da14191ec2e87361cdbbee7a8c0
shoulansi <garrettswhite@gmail.com>
2025-04-28 14:20:50 -0500

    final i named it spring not string lol

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+const stringUtils = require('./springUtils');
 
+stringUtils.capitalize("my name is garrett!");
+stringUtils.reverse("Hello my name is Garrett!!");
+console.log(stringUtils.contains("Hello, I am Garrett!!!!!", "music"));
--- springUtils.js
+++ springUtils.js
@@ -1,1 +1,18 @@
+function capitalize(str){
+    let capitalStr = str.charAt(0).toUpperCase() + str.slice(1);
+    console.log(capitalStr);
+}
 
+function reverse(str) {
+    let reverseStr = str.split("").reverse().join("");
+    console.log(reverseStr);
+}
+
+function contains(str, substr) {
+    if (typeof str !== 'string' || typeof substr !== 'string') {
+        return false;
+    }
+    return str.includes(substr);
+}
+
+module.exports = { capitalize, reverse, contains }
